49279e974799f2a0105a4cf3ffd8c8fe069a67e7
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-05-26 18:03:44 -0500

    adds Open Sans font to project

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button } from './common';
+import { View, Text } from 'react-native';
+// import { Button } from './common';
+
+// const { fontStyle } = styles;
 
 class Login extends Component {
   render() {
     return (
-      <View style={styles.viewPosition}>
-        <Button />
+      <View>
+        <View style={styles.viewPosition}>
+          <Text style={{ fontFamily: 'Open Sans', fontWeight: 'bold' }}>
+            tutti
+          </Text>
+        </View>
+
+        {/* <View style={viewPosition}>
+          <Button />
+        </View> */}
+
+
       </View>
+
     );
   }
 }
 
 const styles = {
   viewPosition: {
-    top: 430,
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center'
-  }
+  },
 };
 
 export default Login;
